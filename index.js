40c215c4e7b58017766253ad3d95ee52dc08f2d1
Tarcisio Liboni <tarcisioliboni@pop-os.localdomain>
2022-06-27 11:54:27 -0300

    Requests 01 e 02 OK

--- index.js
+++ index.js
@@ -1,11 +1,28 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const { readFileSync } = require('fs');
+const { readFile } = require('fs').promises;
 
 const app = express();
 app.use(bodyParser.json());
 
 const HTTP_OK_STATUS = 200;
 const PORT = '3000';
+
+app.get('/talker', (_req, res) => {
+  const talker = JSON.parse(readFileSync('./talker.json', 'utf8'));
+  return res.status(200).send(talker);
+});
+
+app.get('/talker/:id', async (req, res) => {
+  const { id } = req.params;
+  const talkers = JSON.parse(await readFile('./talker.json', 'utf8'));
+  const talker = talkers.find((person) => person.id === Number(id));
+  if (!talker) {
+    return res.status(404).json({ message: 'Pessoa palestrante não encontrada' });
+  }
+  res.status(200).json(talker);
+});
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
